044f2f7d2c110774345911f03ac02fccf7b8cfb3
lang-911 <wangguoguo.lan@gmail.com>
2026-05-10 22:31:50 -0700

    Add --arch flag to build.sh and update shortcuts
    - Support custom target architecture via --arch flag
    - Bump version to v7.0.2-1
    - Add update.shortcut.sh for local deployment and service restart

--- update.shortcut.sh
+++ update.shortcut.sh
@@ -4,7 +4,6 @@ set -u
 
 CPA_VERSION=v7.0.2-1
 
-./build.sh ${CPA_VERSION} \
- && mv cli-proxy-api_${CPA_VERSION}_darwin_amd64 ~/.cli-proxy-api/ \
+ cp /Volumes/L/WebDAV/cpa/cli-proxy-api_${CPA_VERSION}_darwin_amd64 ~/.cli-proxy-api/ \
  && ln -sf ~/.cli-proxy-api/cli-proxy-api_${CPA_VERSION}_darwin_amd64 ~/.cli-proxy-api/cpa \
  && launchctl kickstart -k gui/$(id -u)/com.lang-911.cpa
